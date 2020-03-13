f8f55e42d5159663f47d6580526403e6241cfd63
David Molineus <david.molineus@netzmacht.de>
2020-03-13 14:16:31 +0100

    Add configuration for content element ans modules

--- src/ConsentToolManager.php
+++ src/ConsentToolManager.php
@@ -6,15 +6,41 @@ namespace Hofff\Contao\Consent\Bridge;
 
 final class ConsentToolManager
 {
+    /** @var string[] */
+    private $frontendModules;
+
+    /** @var string[] */
+    private $contentElements;
+
     /** @var ConsentTool|null */
     private $activeConsentTool;
+
+    /**
+     * @param string[] $frontendModules
+     * @param string[] $contentElements
+     */
+    public function __construct(array $frontendModules, array $contentElements)
+    {
+        $this->frontendModules = $frontendModules;
+        $this->contentElements = $contentElements;
+    }
+
+    public function supportedContentElements() : array
+    {
+        return $this->contentElements;
+    }
+
+    public function supportedFrontendModules() : array
+    {
+        return $this->frontendModules;
+    }
 
     public function activate(ConsentTool $consentTool) : void
     {
         $this->activeConsentTool = $consentTool;
     }
 
-    public function isActive() : bool
+    public function hasActiveConsentTool() : bool
     {
         if ($this->activeConsentTool === null) {
             return false;
